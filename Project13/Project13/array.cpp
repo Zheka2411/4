94a11aaf1ea477dba765ee5bfc4eda6bafdc9e03
Zheka2411 <131461545+Zheka2411@users.noreply.github.com>
2024-12-23 14:24:13 +0000

    Add files via upload

---
+++ Project13/Project13/array.cpp
@@ -0,0 +1,143 @@
+﻿#include <iostream>
+
+template <typename T>
+class Array {
+private:
+    T* data;
+    size_t size;
+
+public:
+    // Конструктор
+    Array(size_t size = 0) : size(size) {
+        data = new T[size];
+    }
+
+    // Конструктор копіювання
+    Array(const Array& other) : size(other.size) {
+        data = new T[size];
+        for (size_t i = 0; i < size; ++i) {
+            data[i] = other.data[i];
+        }
+    }
+
+    // Оператор = (глибоке копіювання)
+    Array& operator=(const Array& other) {
+        if (this != &other) {
+            delete[] data;
+            size = other.size;
+            data = new T[size];
+            for (size_t i = 0; i < size; ++i) {
+                data[i] = other.data[i];
+            }
+        }
+        return *this;
+    }
+
+    // Оператор + (поелементне об'єднання)
+    Array operator+(const Array& other) const {
+        size_t newSize = size + other.size;
+        Array newArray(newSize);
+        for (size_t i = 0; i < size; ++i) {
+            newArray.data[i] = data[i];
+        }
+        for (size_t i = 0; i < other.size; ++i) {
+            newArray.data[size + i] = other.data[i];
+        }
+        return newArray;
+    }
+
+    // Оператор += (поелементне об'єднання з присвоєнням)
+    Array& operator+=(const Array& other) {
+        size_t newSize = size + other.size;
+        T* newData = new T[newSize];
+        for (size_t i = 0; i < size; ++i) {
+            newData[i] = data[i];
+        }
+        for (size_t i = 0; i < other.size; ++i) {
+            newData[size + i] = other.data[i];
+        }
+        delete[] data;
+        data = newData;
+        size = newSize;
+        return *this;
+    }
+
+    // Оператор [] (доступ до елементу по індексу)
+    T& operator[](size_t index) {
+        return data[index];
+    }
+
+    // Оператор << (виведення масиву в консоль)
+    friend std::ostream& operator<<(std::ostream& os, const Array& arr) {
+        for (size_t i = 0; i < arr.size; ++i) {
+            os << arr.data[i] << " ";
+        }
+        return os;
+    }
+
+    // Оператор == (поелементне порівняння)
+    bool operator==(const Array& other) const {
+        if (size != other.size) return false;
+        for (size_t i = 0; i < size; ++i) {
+            if (data[i] != other.data[i]) return false;
+        }
+        return true;
+    }
+
+    // Оператор != (поелементне порівняння на нерівність)
+    bool operator!=(const Array& other) const {
+        return !(*this == other);
+    }
+
+    // Оператор > (порівняння за довжиною)
+    bool operator>(const Array& other) const {
+        return size > other.size;
+    }
+
+    // Оператор < (порівняння за довжиною)
+    bool operator<(const Array& other) const {
+        return size < other.size;
+    }
+
+    // Оператор * (спільні елементи)
+    Array operator*(const Array& other) const {
+        size_t minSize = std::min(size, other.size);
+        Array commonArray(minSize);
+        size_t index = 0;
+        for (size_t i = 0; i < size; ++i) {
+            for (size_t j = 0; j < other.size; ++j) {
+                if (data[i] == other.data[j]) {
+                    commonArray.data[index++] = data[i];
+                }
+            }
+        }
+        return commonArray;
+    }
+
+    // Деструктор
+    ~Array() {
+        delete[] data;
+    }
+};
+
+int main() {
+    Array<int> arr1(5);
+    Array<int> arr2(3);
+
+    for (int i = 0; i < 5; ++i) arr1[i] = i + 1;
+    for (int i = 0; i < 3; ++i) arr2[i] = i + 4;
+
+    Array<int> arr3 = arr1 + arr2; // поелементне об'єднання
+    std::cout << arr3 << std::endl;
+
+    arr1 += arr2; // поелементне об'єднання з присвоєнням
+    std::cout << arr1 << std::endl;
+
+    std::cout << (arr1 == arr2) << std::endl; // порівняння на рівність
+    std::cout << (arr1 > arr2) << std::endl;  // порівняння за довжиною
+
+    Array<int> common = arr1 * arr2; // спільні елементи
+    std::cout << common << std::endl;
+
+    return 0;
+}
---
+++ Project13/Project13/string.cpp
@@ -0,0 +1,102 @@
+﻿#include <iostream>
+#include <cstring>
+
+class String {
+private:
+    char* data;
+public:
+    // Конструктор
+    String(const char* str = "") {
+        data = new char[strlen(str) + 1];
+        strcpy(data, str);
+    }
+
+    // Конструктор копіювання
+    String(const String& other) {
+        data = new char[strlen(other.data) + 1];
+        strcpy(data, other.data);
+    }
+
+    // Оператор = (глибоке копіювання)
+    String& operator=(const String& other) {
+        if (this != &other) {
+            delete[] data;
+            data = new char[strlen(other.data) + 1];
+            strcpy(data, other.data);
+        }
+        return *this;
+    }
+
+    // Оператор + (конкатенація рядків)
+    String operator+(const String& other) const {
+        size_t newLength = strlen(data) + strlen(other.data) + 1;
+        char* newData = new char[newLength];
+        strcpy(newData, data);
+        strcat(newData, other.data);
+        return String(newData);
+    }
+
+    // Оператор += (конкатенація з присвоєнням)
+    String& operator+=(const String& other) {
+        size_t newLength = strlen(data) + strlen(other.data) + 1;
+        char* newData = new char[newLength];
+        strcpy(newData, data);
+        strcat(newData, other.data);
+        delete[] data;
+        data = newData;
+        return *this;
+    }
+
+    // Оператор [] (доступ до символу по індексу)
+    char operator[](size_t index) const {
+        return data[index];
+    }
+
+    // Оператор << (виведення рядка в консоль)
+    friend std::ostream& operator<<(std::ostream& os, const String& str) {
+        os << str.data;
+        return os;
+    }
+
+    // Оператор == (порівняння на рівність)
+    bool operator==(const String& other) const {
+        return strcmp(data, other.data) == 0;
+    }
+
+    // Оператор != (порівняння на нерівність)
+    bool operator!=(const String& other) const {
+        return strcmp(data, other.data) != 0;
+    }
+
+    // Оператор > (порівняння за довжиною)
+    bool operator>(const String& other) const {
+        return strlen(data) > strlen(other.data);
+    }
+
+    // Оператор < (порівняння за довжиною)
+    bool operator<(const String& other) const {
+        return strlen(data) < strlen(other.data);
+    }
+
+    // Деструктор
+    ~String() {
+        delete[] data;
+    }
+};
+
+int main() {
+    String str1("Hello");
+    String str2("World");
+    String str3 = str1 + str2; // конкатенація
+    std::cout << str3 << std::endl;
+
+    str1 += str2; // +=
+    std::cout << str1 << std::endl;
+
+    std::cout << (str1 == str2) << std::endl; // порівняння на рівність
+    std::cout << (str1 > str2) << std::endl;  // порівняння за довжиною
+
+    std::cout << str1[1] << std::endl; // доступ до символу
+
+    return 0;
+}
